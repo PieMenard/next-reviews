62603871a651d6495efe704e248f2603f112aebd
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-06-14 13:45:34 -0400

    set mockup pages

---
+++ src/app/reviews/hollow-knight/page.tsx
@@ -0,0 +1,8 @@
+export default function HollowKnightPage() {
+  return (
+    <>
+      <h1>Hollow Knight Review</h1>
+      <p>Placeholder text</p>
+    </>
+  );
+}
---
+++ src/app/reviews/stardew-valley/page.tsx
@@ -0,0 +1,8 @@
+export default function StardewValleyPage() {
+  return (
+    <>
+      <h1>Stardew Valley Review</h1>
+      <p>Placeholder text</p>
+    </>
+  );
+}
