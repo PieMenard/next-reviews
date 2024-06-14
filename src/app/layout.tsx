390ec34f2dbc9461dc20acb2d62cac8e367d3656
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-06-14 15:32:24 -0400

    add links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,9 +18,28 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <header>[header]</header>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link href={'/'}>Home</Link>
+              </li>
+              <li>
+                <Link href={'/reviews'}>Reviews</Link>
+              </li>
+              <li>
+                <Link href={'/about'}>About</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
         <main>{children}</main>
-        <footer>[footer]</footer>
+        <footer>
+          Game data and images courtesy of{' '}
+          <a href="https://rawg.io" target="_blank">
+            RAWG
+          </a>
+        </footer>
       </body>
     </html>
   );
