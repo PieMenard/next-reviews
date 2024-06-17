275f8cc3b84fdb681079c3edbf52b3df6446194a
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-06-17 14:12:39 -0400

    render reviews list

--- src/app/reviews/page.tsx
+++ src/app/reviews/page.tsx
@@ -1,40 +1,32 @@
 import Heading from '@/components/Heading';
+import { getReviews } from '@/lib/reviews';
 import Link from 'next/link';
 
-export default function ReviewsPage() {
+export default async function ReviewsPage() {
+  const reviews = await getReviews();
   return (
     <>
       <Heading>Reviews</Heading>
-      <p>List of reviews</p>
-      <ul className="flex flex-col gap-3">
-        <li className="bg-white/70 border rounded shadow w-80 hover:shadow-xl">
-          <Link href={'/reviews/stardew-valley'}>
-            <img
-              src="/images/stardew-valley.jpg"
-              alt=""
-              width="320"
-              height="180"
-              className="rounded-t"
-            />
-            <h2 className="font-orbitron font-bold py-1 text-center">
-              Stardew Valley
-            </h2>
-          </Link>
-        </li>
-        <li className="bg-white/70 border rounded shadow w-80 hover:shadow-xl">
-          <Link href={'/reviews/hollow-knight'}>
-            <img
-              src="/images/hollow-knight.jpg"
-              alt=""
-              width="320"
-              height="180"
-              className="rounded-t"
-            />
-            <h2 className="font-orbitron font-bold py-1 text-center">
-              Hollow Knight
-            </h2>
-          </Link>
-        </li>
+      <ul className="flex flex-row flex-wrap gap-3">
+        {reviews.map((review) => (
+          <li
+            key={review.slug}
+            className="bg-white/70 border rounded shadow w-80 hover:shadow-xl"
+          >
+            <Link href={`/reviews/${review.slug}`}>
+              <img
+                src={review.image}
+                alt=""
+                width="320"
+                height="180"
+                className="rounded-t"
+              />
+              <h2 className="font-orbitron font-bold py-1 text-center">
+                {review.title}
+              </h2>
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
   );
--- src/lib/reviews.ts
+++ src/lib/reviews.ts
@@ -1,20 +1,32 @@
-import { readFile } from 'fs/promises';
+import { readdir, readFile } from 'fs/promises';
 import matter from 'gray-matter';
 import { marked } from 'marked';
 
 type Review = {
+  slug: string;
   title: string;
   date: string;
   image: string;
   body: string;
 }
 
-export async function getReview(slug: string) {
+export async function getReview(slug: string): Promise<Review> {
   const text = await readFile(`./content/reviews/${slug}.md`, 'utf8');
   const {
     content,
     data: { title, date, image },
   } = matter(text);
-  const body = marked(content);
-  return { title, date, image, body };
+  const body = marked(content) as string;
+  return { slug, title, date, image, body };
+}
+
+export async function getReviews(): Promise<Review[]> {
+  const files = await readdir('./content/reviews')
+  const slugs = files.filter((file) => file.endsWith('md')).map((file) => file.slice(0, -'.md'.length))
+  const reviews: Review[] = [];
+  for (const slug of slugs) {
+    const review = await getReview(slug)
+    reviews.push(review)
+  }
+  return reviews
 }
