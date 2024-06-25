a6aa669a0cc96c28779b0207e0c5b21987f218a8
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-06-25 09:35:03 -0400

    implement configurable image source

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,19 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '1337',
-        pathname: '/uploads/**',
-      },
-    ],
+    remotePatterns: [toRemotePattern(process.env.CMS_IMAGE_PATTERN)],
   },
 };
 
 export default nextConfig;
+
+function toRemotePattern(urlString) {
+  const url = new URL(urlString);
+  return {
+    protocol: url.protocol.replace(':', ''),
+    hostname: url.hostname,
+    port: url.port,
+    pathname: url.pathname,
+  };
+}
