9d535afe0a8734c10fb5bfe3956911ecadccab1c
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-06-25 16:29:35 -0400

    fix prisma client and dev server issue

--- src/lib/comments.ts
+++ src/lib/comments.ts
@@ -1,8 +1,16 @@
-import { db } from "./db";
+import type { Comment } from '@prisma/client';
+import { db } from './db';
+
+export type CreateCommentProps = Omit<Comment, 'id' | 'postedAt'>;
+
+export async function createComment({ slug, user, message }: CreateCommentProps) {
+    return await db.comment.create({
+        data: { slug, user, message },
+    });
+}
 
 export async function getComments(slug: string) {
     return await db.comment.findMany({
         where: { slug },
     });
-
 }
